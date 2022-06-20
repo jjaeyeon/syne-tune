1be9efeec272abc4e722a0cdb0887aa58ce96ae3
Jaeyeon <jaeyeon@amazon.de>
2022-06-20 11:53:46 +0000

    added __all__ for use

--- syne_tune/backend/__init__.py
+++ syne_tune/backend/__init__.py
@@ -11,22 +11,18 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-__all__ = []
-
+__all__ = ["LocalBackend", "SageMakerBackend", "PythonBackend"]
 try:
     from syne_tune.backend.local_backend import LocalBackend
-    __all__.append("LocalBackend")
 except ImportError:
-    pass
+    __all__.remove("LocalBackend")
 
 try:
     from syne_tune.backend.python_backend.python_backend import PythonBackend
-    __all__.append("PythonBackend")
 except ImportError:
-    pass
+    __all__.remove("PythonBackend")
 
 try:
     from syne_tune.backend.sagemaker_backend.sagemaker_backend import SageMakerBackend
-    __all__.append("SageMakerBackend")
 except ImportError:
-    pass
+    __all__.remove("SageMakerBackend")
